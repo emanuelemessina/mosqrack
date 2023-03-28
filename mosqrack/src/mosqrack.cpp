cc3e1843b5e58e76077b638220733eb11f9524c8
Emanuele Messina <emanuelemessina.em@gmail.com>
2023-03-28 14:37:24 +0200

    chunking logic added timer fixed thread sync fixed console_log

--- mosqrack/src/mosqrack.cpp
+++ mosqrack/src/mosqrack.cpp
@@ -1,13 +1,27 @@
-﻿#pragma warning(disable : 6031)
+﻿/*
+Copyright (c) 2023 Emanuele Messina <emanuelemessina.em@gmail.com>
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License 2.0
+and Eclipse Distribution License v1.0 which accompany this distribution.
+The Eclipse Public License is available at
+   https://www.eclipse.org/legal/epl-2.0/
+and the Eclipse Distribution License is available at
+  http://www.eclipse.org/org/documents/edl-v10.php.
+SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+*/
+
+#pragma warning(disable : 6031)
 #pragma warning(push)
 
 #include "mosqrack.h"
+#include "timer.h"
 
 #include "mosquitto/password_mosq.h"
 #include "mosquitto/mosquitto_passwd.h"
 
 #define NUM_THREADS 4
 #define MAX_PATH_LEN 255
+#define MAX_LINE_LEN 255
 
 std::atomic<bool> hash_match; // thread sync
 std::mutex thread_mtx;
@@ -15,76 +29,62 @@ bool thread_status[NUM_THREADS];
 
 struct hash_match_args {
 	int thread_id;
-	std::string wordlist_path;
-	size_t num_lines;
+	std::stringstream wordlist_chunk;
 	const char* username;
 	char* salt64;
 	const char* desiredOutput;
 };
 
-void hashMatch(hash_match_args args) {
+void hashMatch(hash_match_args* args) {
 	
+	// set working status
 	thread_mtx.lock();
-	thread_status[args.thread_id] = true; // i'm working
+	thread_status[args->thread_id] = true; // i'm working
 	thread_mtx.unlock();
 
-	// open wordlist
-	std::ifstream hWordlist;
-	hWordlist.open(args.wordlist_path);
-
-	// position seek to starting line
-
-	size_t starting_line = args.thread_id * args.num_lines;
-	
-	if (starting_line > 0) {
-		for (int i = 0; i < starting_line; i++) {
-			hWordlist.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		}
-	}
-
-	size_t current_line = starting_line;
-	size_t last_line = starting_line + args.num_lines -1;
-
 	std::string wpass;
-	while (current_line <= last_line && std::getline(hWordlist, wpass)) {
+	while (std::getline(args->wordlist_chunk, wpass)) {
 		
 		// thread sync
 		if (hash_match) {
 			thread_mtx.lock();
-			thread_status[args.thread_id] = false; // i exited
+			thread_status[args->thread_id] = false; // i exited
 			thread_mtx.unlock();
 			return;
 		}
 		
 		char output[MAX_BUFFER_LEN];
-		output_new_password(output, args.username, wpass.c_str(), args.salt64, PW_DEFAULT_ITERATIONS);
+		output_new_password(output, args->username, wpass.c_str(), args->salt64, PW_DEFAULT_ITERATIONS);
 		
-		if (strncmp(args.desiredOutput, output, strlen(output)-1) == 0) {
-			console_log("Hash match!", "Password:", wpass);
-			// thread sync
+		if (strncmp(args->desiredOutput, output, strlen(output)-1) == 0) { // output contains a new line
+			
+			console_log("Hash match from thread[", args->thread_id, "]", "Password:", wpass);
+			
+			// set found and exited status
 			hash_match = true;
 			thread_mtx.lock();
-			thread_status[args.thread_id] = false; // i exited
+			thread_status[args->thread_id] = false; // i exited
 			thread_mtx.unlock();
+
 			return;
 		}
-
-		current_line++;
 	}
 
 	// not found
-	// 
-	// thread sync
+
+	// set exited status
 	thread_mtx.lock();
-	thread_status[args.thread_id] = false; // i exited
+	thread_status[args->thread_id] = false; // i exited
 	thread_mtx.unlock();
 }
 
 int main(int argc, char** argv)
 {
+	// check args
+
 	std::string help =
 		R"(Usage:
-	moscrytto <hashfile> <wordlist>
+	moscrytto <passwd_file> <wordlist_file>
 	)";
 
 	if (argc != 3) {
@@ -97,6 +97,8 @@ int main(int argc, char** argv)
 
 	sprintf_s(hashfile, MAX_PATH_LEN, "%s", argv[1]);
 	sprintf_s(wordlist, MAX_PATH_LEN, "%s", argv[2]);
+
+	// open files
 
 	ASSERT_FILE_EXISTS(hashfile);
 	OTHERWISE(ABORT(1, "Aborted:", hashfile, "does not exist."));
@@ -111,61 +113,122 @@ int main(int argc, char** argv)
 	OPEN_FSTREAM_AND_ASSERT_SUCCESS(hWordlist, wordlist, std::ios::in);
 	OTHERWISE(ABORT(1, "Error opening", wordlist));
 
+	// process files
+
 	std::string hashfile_line;
 	std::getline(hHashfile, hashfile_line);
 	
 	std::string username = hashfile_line.substr(0, hashfile_line.find(":"));
 	
-	char hflb[255];
+	char hflb[MAX_LINE_LEN];
 	hashfile_line.copy(hflb, hashfile_line.length());
 	strtok(hflb, "$");
 	strtok(NULL, "$");
 
 	char* salt64 = strtok(NULL, "$");
 
-	console_log("Preparing threads...");
-	size_t wordlist_lines = std::count(std::istreambuf_iterator<char>(hWordlist),
-	std::istreambuf_iterator<char>(), '\n');
+	size_t worlist_size = std::filesystem::file_size(wordlist);
+	size_t per_thread_wordlist_chunk_size = worlist_size / NUM_THREADS;
+		
+	// start timer
+	
+	Timer<std::chrono::seconds, std::chrono::steady_clock> clock;
+	clock.tick();
 
-	size_t num_lines_per_thread = wordlist_lines / NUM_THREADS;
-	size_t remainder_lines = wordlist_lines % NUM_THREADS;
-
-	// set all threads as terminated
-	for (int i = 0; i < NUM_THREADS; i++) {
-		thread_status[i] = false;
-	}
 	// spawn threads
+	
+	console_log("Preparing threads...");
+
 	std::vector<std::thread> threads;
-	for (int i = 0; i < NUM_THREADS; i++) {
-		hash_match_args args;
-		args.thread_id = i;
-		args.wordlist_path = wordlist;
-		args.num_lines = num_lines_per_thread + (i == NUM_THREADS - 1 ? remainder_lines : 0); // add remainder lines for last thread
-		args.username = username.c_str();
-		args.salt64 = salt64;
-		args.desiredOutput = hashfile_line.c_str();
+	std::vector<hash_match_args*> h_args;
+	int currThreadId = 0;
+	
+	for (; currThreadId < NUM_THREADS; currThreadId++) {
+
+		// set all threads as terminated
+		thread_status[currThreadId] = false;
+
+		// hold stop spawning condition
+		bool noMoreThreads = false;
+
+		// populate thread args
+		h_args.push_back(new hash_match_args);
+		hash_match_args* args = h_args[currThreadId];
+
+		args->thread_id = currThreadId;
+		args->username = username.c_str();
+		args->salt64 = salt64;
+		args->desiredOutput = hashfile_line.c_str();
+
+		// read chunk and store in arg
+		// fstream will increment file pointer so next thread will begin from first line of next chunk
+		// in particular the last thread will take care of reading until eof
+				
+		char* read_chunk = (char*) malloc((per_thread_wordlist_chunk_size+1)*sizeof(char)); // alloc chunck size
+		if (!hWordlist.read(read_chunk, per_thread_wordlist_chunk_size)) { noMoreThreads = true; } // read per thread chunk size
+		read_chunk[hWordlist.gcount()] = 0; // null terminate buff
+		args->wordlist_chunk << read_chunk; // append read to chunk buff content
+		
+		read_chunk = (char*) malloc(MAX_LINE_LEN * sizeof(char)); // alloc a line
+		if (!hWordlist.getline(read_chunk, MAX_LINE_LEN)) { noMoreThreads = true; } // read until new line
+		read_chunk[hWordlist.gcount()] = 0;
+		args->wordlist_chunk << read_chunk;
+
+		if (currThreadId == NUM_THREADS - 1) { // last thread -> read until eof 
+			while (hWordlist.getline(read_chunk, MAX_LINE_LEN)) { // read_chunk is already allocated with MAX_LINE_LEN
+				read_chunk[hWordlist.gcount()] = 0;
+				args->wordlist_chunk << read_chunk;
+			}
+		}
+		
+		// start and push thread
 		threads.push_back(std::thread(hashMatch, args));
+
+		// check whether to stop spwaning threads
+		if (noMoreThreads) {
+			break;
+		}
 	}
 
-	console_log("Threads spawned. Searching...");
+	console_log("Spawned", currThreadId+1, "threads.", "Searching...");
 
-	// exit conditions
+	// main thread: check for exit conditions
+
 	while (true) {
+
 		// check if thread found match
-		if (hash_match)
+		
+		if (hash_match) {
+			clock.tock();
+			console_log("Elapsed:", clock.elapsed(), "s.");
+			// all threads will terminate upon reading the match status
+			for (int i = 0; i < NUM_THREADS; i++) {
+				threads[i].join();
+			}
 			return 0;
+		}
+		
 		// check if none found
+		
 		bool stillWorking = false;
+		
 		for (int i = 0; i < NUM_THREADS; i++) {
+		
 			thread_mtx.lock();
+
 			if (thread_status[i]) { // thread still working
 				stillWorking = true;
 				thread_mtx.unlock();
 				break;
 			}
+
 			thread_mtx.unlock();
 		}
-		if (!stillWorking) // all thread exited
+		
+		if (!stillWorking) { // all thread exited
+			clock.tock();
+			console_log("Elapsed:", clock.elapsed());
 			ABORT(0, "None found, exiting.");
+		}
 	}
 }
--- mosqrack/src/mosqrack.h
+++ mosqrack/src/mosqrack.h
@@ -1,4 +1,16 @@
-﻿#pragma once
+﻿/*
+Copyright (c) 2023 Emanuele Messina <emanuelemessina.em@gmail.com>
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License 2.0
+and Eclipse Distribution License v1.0 which accompany this distribution.
+The Eclipse Public License is available at
+   https://www.eclipse.org/legal/epl-2.0/
+and the Eclipse Distribution License is available at
+  http://www.eclipse.org/org/documents/edl-v10.php.
+SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+*/
+
+#pragma once
 
 #include <iostream>
 #include <stdio.h>
@@ -9,15 +21,33 @@
 #include <thread>
 #include <mutex>
 
-inline void console_log() {
+// private overloads
+inline void _console_log() {
 	std::cout << std::endl;
 }
+inline void _console_log(std::string str) {
+	std::cout << str.c_str();
+}
+template <typename T>
+inline void _console_log(T unk) {
+	std::cout << unk;
+}
 
-template <typename... OtherMsgs>
-inline void console_log(std::string msg, OtherMsgs... msgs)
+// public
+
+// single item
+template <typename Tf>
+inline void console_log(Tf item)
 {
-	std::cout << msg.c_str() << " ";
-
+	_console_log(item);
+	_console_log();
+}
+// variadic
+template <typename Tf, typename ...Ts>
+inline void console_log(Tf msg, Ts... msgs)
+{
+	_console_log(msg);
+	_console_log(" ");
 	console_log(msgs...);
 }
 
